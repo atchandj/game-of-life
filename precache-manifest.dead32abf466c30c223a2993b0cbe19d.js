b2f6423f0e321d1b82e3ece806bcaa99fbed25ac
Adrien Tchandjou <atchandjou@gmail.com>
2021-12-09 22:06:07 +0100

    Updates

---
+++ precache-manifest.dead32abf466c30c223a2993b0cbe19d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4e6765c3778948fc1d700709ff05bd",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "e8243cf1da23a53547c6",
+    "url": "/game-of-life/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "57fd9d586c3ff903dd6f",
+    "url": "/game-of-life/static/css/main.cd74945e.chunk.css"
+  },
+  {
+    "revision": "e8243cf1da23a53547c6",
+    "url": "/game-of-life/static/js/2.cea9b341.chunk.js"
+  },
+  {
+    "revision": "57fd9d586c3ff903dd6f",
+    "url": "/game-of-life/static/js/main.9e5cbda2.chunk.js"
+  },
+  {
+    "revision": "6c337d97cb58ee876942",
+    "url": "/game-of-life/static/js/runtime-main.51be29ed.js"
+  }
+]);
